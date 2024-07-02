040e97fec79a019192cf974a0d62d1269c0bd11c
sava4632 <safevaar@gmail.com>
2024-07-02 17:00:23 +0200

    First Commit :)

---
+++ src/test/java/com/sava4632/todo_list/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sava4632.todo_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
