7f7c04a34a32af1757a09a3c6743d5ecf88b76f2
sava4632 <safevaar@gmail.com>
2024-08-16 01:42:00 +0200

    Add the github url in the corns

--- src/main/java/com/sava4632/todo_list/TodoListApplication.java
+++ src/main/java/com/sava4632/todo_list/TodoListApplication.java
@@ -3,11 +3,27 @@ package com.sava4632.todo_list;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class TodoListApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoListApplication.class, args);
+	}
+}
+
+@Configuration
+class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("https://todo-list-angular-production.up.railway.app")
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+				.allowedHeaders("*")
+				.allowCredentials(true);
 	}
 }
--- src/main/java/com/sava4632/todo_list/model/payload/WebConfig.java
+++ src/main/java/com/sava4632/todo_list/model/payload/WebConfig.java
@@ -1,38 +1,38 @@
-package com.sava4632.todo_list.model.payload;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.lang.NonNull;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(@NonNull CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200", "https://todo-list-angular-production.up.railway.app")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("Authorization", "Content-Type", "Accept", "Origin", "X-Requested-With")
-                .allowCredentials(true);
-
+//package com.sava4632.todo_list.model.payload;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import org.springframework.lang.NonNull;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(@NonNull CorsRegistry registry) {
 //        registry.addMapping("/**")
-//                .allowedOrigins("http://localhost:4200", "https://sava4632.github.io")
-//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                .allowedHeaders("Authorization", "Content-Type", "Accept", "Origin", "X-Requested-With")
-//                .allowCredentials(true);
-
-//        registry.addMapping("/**")
-//                .allowedOrigins("http://localhost:4200")
+//                .allowedOrigins("https://todo-list-angular-production.up.railway.app")
 //                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 //                .allowedHeaders("*")
 //                .allowCredentials(true);
-
-//        registry.addMapping("/**")
-//                .allowedOrigins("*") // Permite todos los orígenes
-//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
-//                .allowedHeaders("*") // Permite todos los encabezados
-//                .allowCredentials(false); // Las credenciales no se permiten si se usa "*"
-    }
-}
-
+//
+////        registry.addMapping("/**")
+////                .allowedOrigins("http://localhost:4200", "https://sava4632.github.io")
+////                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+////                .allowedHeaders("Authorization", "Content-Type", "Accept", "Origin", "X-Requested-With")
+////                .allowCredentials(true);
+//
+////        registry.addMapping("/**")
+////                .allowedOrigins("http://localhost:4200")
+////                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+////                .allowedHeaders("*")
+////                .allowCredentials(true);
+//
+////        registry.addMapping("/**")
+////                .allowedOrigins("*") // Permite todos los orígenes
+////                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+////                .allowedHeaders("*") // Permite todos los encabezados
+////                .allowCredentials(false); // Las credenciales no se permiten si se usa "*"
+//    }
+//}
+//
