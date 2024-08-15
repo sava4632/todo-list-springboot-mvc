c3e3d0a66105e628fa5eb38cc4f4cde2057cb239
sava4632 <safevaar@gmail.com>
2024-08-16 01:34:19 +0200

    Add the github url in the corns

--- src/main/java/com/sava4632/todo_list/model/payload/WebConfig.java
+++ src/main/java/com/sava4632/todo_list/model/payload/WebConfig.java
@@ -11,10 +11,16 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200", "https://sava4632.github.io")
+                .allowedOrigins("http://localhost:4200", "https://todo-list-angular-production.up.railway.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Authorization", "Content-Type", "Accept", "Origin", "X-Requested-With")
                 .allowCredentials(true);
+
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:4200", "https://sava4632.github.io")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                .allowedHeaders("Authorization", "Content-Type", "Accept", "Origin", "X-Requested-With")
+//                .allowCredentials(true);
 
 //        registry.addMapping("/**")
 //                .allowedOrigins("http://localhost:4200")
