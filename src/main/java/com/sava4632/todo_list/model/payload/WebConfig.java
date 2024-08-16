7bdfe7dd73d3cfa773ff29a0e47cd4c254bacd6a
sava4632 <safevaar@gmail.com>
2024-08-16 17:48:24 +0200

    Add the github url in the corns

--- src/main/java/com/sava4632/todo_list/model/payload/WebConfig.java
+++ src/main/java/com/sava4632/todo_list/model/payload/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://todolist-sava4632.netlify.app", "https://sava4632.github.io/","http://localhost:4200/")
+                .allowedOrigins("https://todolist-sava4632.netlify.app", "https://sava4632.github.io","http://localhost:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
